2994a95a1b20d5a1e8b87d26c18c8adcf4679de1
Alexandr <alexandrsinko2000@gmail.com>
2024-08-21 11:21:52 +0300

    small refactor main class

--- src/main/java/com/exchangerate/app/ExchangeRateAppApplication.java
+++ src/main/java/com/exchangerate/app/ExchangeRateAppApplication.java
@@ -7,9 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class ExchangeRateAppApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ExchangeRateAppApplication.class, args);
 	}
-
 }
